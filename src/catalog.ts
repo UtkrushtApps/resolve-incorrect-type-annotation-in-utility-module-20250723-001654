65ba8e0d306269afec26836b8d1d7049d987f9d1
Naman Muley <naman.g.muley@gmail.com>
2025-07-23 00:17:12 +0530

    Add src/catalog.ts

---
+++ src/catalog.ts
@@ -0,0 +1,9 @@
+import { Product } from './types/Product';
+
+export const products: Product[] = [
+  { id: 1, name: 'Laptop', price: 1000 },
+  { id: 2, name: 'Mouse', price: 25 },
+  { id: 3, name: 'Mystery Box' },
+  { id: 4, name: 'Keyboard', price: 55 },
+  { id: 5, name: 'Surprise Item' }
+];
